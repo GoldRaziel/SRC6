c761f827e09e1a269b8ba09cc997d9156a27c694
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 21:48:45 +0100

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,14 +7,24 @@ const About = () => {
 
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
+      {/* Titolo */}
       <h1 style={{ fontSize: '2.5rem', color: '#00ccff' }}>
         {t('about_title')}
       </h1>
 
-      <p style={{ fontSize: '1.1rem', maxWidth: '900px', margin: '1.5rem auto', lineHeight: '1.8', whiteSpace: 'pre-line' }}>
-        {t('about_text')}
-      </p>
+      {/* Testo con HTML inline dal translation.json */}
+      <p
+        style={{
+          fontSize: '1.1rem',
+          maxWidth: '900px',
+          margin: '1.5rem auto',
+          lineHeight: '1.8',
+          textAlign: 'center'
+        }}
+        dangerouslySetInnerHTML={{ __html: t('about_text') }}
+      />
 
+      {/* Immagine Freddy ologramma */}
       <img
         src={freddy}
         alt="Freddy Mercury Ologramma"
