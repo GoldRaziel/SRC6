0b6b333c3ae780fe089490f60ce6ff0e062c243a
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 19:59:52 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import logo from '../assets/images/1. Esterno notte.png';
+import mainImage from '../assets/images/1. Esterno notte.png';
+import logo from '../assets/images/soulrock-logo.png'; // nuovo logo fornito
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1 style={{ fontSize: '2.5rem', marginBottom: '1rem' }}>
-        {t('home_title').split(' ')[0]}<br />
-        <img src={logo} alt="Soul Rock Logo" style={{ maxWidth: '300px', marginTop: '10px', borderRadius: '10px' }} />
+      {/* Immagine grande */}
+      <img src={mainImage} alt="Soul Rock Exterior" style={{ maxWidth: '100%', borderRadius: '10px' }} />
+
+      {/* Titolo + Logo */}
+      <h1 style={{ fontSize: '2.5rem', marginTop: '2rem' }}>
+        Benvenuti al
+        <br />
+        <img
+          src={logo}
+          alt="Soul Rock Logo"
+          style={{ maxWidth: '300px', marginTop: '10px' }}
+        />
       </h1>
+
+      {/* Testo descrittivo */}
       <p style={{ fontSize: '1.2rem', maxWidth: '700px', margin: '0 auto' }}>
         {t('home_text')}
       </p>
+
+      {/* Pulsanti */}
       <div style={{ marginTop: '2rem' }}>
         <a href="/idea"><button>{t('home_cta_discover')}</button></a>
         <a href="/donate"><button>{t('home_cta_donate')}</button></a>
