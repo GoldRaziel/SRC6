8a9cb2b09628f1894f20f18bff3cb0b7971b35d9
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 21:17:01 +0100

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import freddy from '../assets/images/freddy-ologramma.png';
 
 const About = () => {
   const { t } = useTranslation();
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>{t('about_title')}</h1>
-      <p>{t('about_text')}</p>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '2.5rem', color: '#00ccff' }}>
+        {t('about_title')}
+      </h1>
+
+      <p style={{ fontSize: '1.1rem', maxWidth: '900px', margin: '1.5rem auto', lineHeight: '1.8' }}>
+        {t('about_text')}
+      </p>
+
+      <img
+        src={freddy}
+        alt="Freddy Mercury Ologramma"
+        style={{
+          maxWidth: '400px',
+          marginTop: '2rem',
+          borderRadius: '10px',
+          boxShadow: '0 0 20px #00ccff'
+        }}
+      />
     </div>
   );
 };
