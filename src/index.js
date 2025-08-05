a490ee231891d500a1dacc30a8c1c22d6300cc50
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 22:49:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'yet-another-react-lightbox/styles.css'; // ✅ Importa lo stile del lightbox
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
