0594887b85055e29ea292c8996ebfcaa3002aa17
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 20:08:25 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import logo from '../assets/images/1. Esterno notte.png';
+import image from '../assets/images/1. Esterno notte.png';
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
+      {/* Immagine grande */}
       <img
-        src={logo}
+        src={image}
         alt="Soul Rock Exterior"
         style={{ maxWidth: '100%', borderRadius: '10px' }}
       />
-      <h1 style={{ fontSize: '2.5rem', marginTop: '2rem' }}>
-        {t('home_title')}
+
+      {/* Titolo in tre righe */}
+      <h1 style={{ fontSize: '2.5rem', marginTop: '2rem', color: '#00ccff', lineHeight: '1.5' }}>
+        Benvenuti al<br />
+        SOUL ROCK<br />
+        MUSEUM RESTAURANT
       </h1>
+
+      {/* Frase */}
       <p style={{ fontSize: '1.2rem', maxWidth: '700px', margin: '0 auto' }}>
         {t('home_text')}
       </p>
+
+      {/* Pulsanti */}
       <div style={{ marginTop: '2rem' }}>
         <a href="/idea"><button>{t('home_cta_discover')}</button></a>
         <a href="/donate"><button>{t('home_cta_donate')}</button></a>
