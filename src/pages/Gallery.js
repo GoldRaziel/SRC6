b01625931e865fcd63c51fbaacc796762ea5a73a
GoldRaziel <paolocimino21@gmail.com>
2025-08-06 00:13:18 +0100

    Update Gallery.js

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,23 +1,32 @@
-// src/components/Gallery.js
 import React, { useState } from 'react';
 import './Gallery.css';
 
-import img1 from '../assets/images/1. Esterno notte.png';
-import img2 from '../assets/images/2. Reception.png';
+import img1 from '../assets/images/1.Esterno-notte.png';
+import img2 from '../assets/images/2.Reception.png';
 import img3 from '../assets/images/3. Attesa reception.png';
 import img4 from '../assets/images/4. Sala ED209.png';
-import img5 from '../assets/images/5. Sala Robocop.png';
-import img6 from '../assets/images/6. Sala Cocktail Michael Jackson.png';
-import img7 from '../assets/images/7. Bancone Birra T800 Skeleton.png';
-import img8 from '../assets/images/8. Cucina a Vista.png';
-import img9 from '../assets/images/9. Cucina Hamburger.png';
-import img10 from '../assets/images/10. Parete in Jeans.png';
-import img11 from '../assets/images/11. Shop Soul Rock.png';
+import img5 from '../assets/images/5. Sala Cocktail Michael Jackson.png';
+import img6 from '../assets/images/6. Bancone Birra T800 Skeleton.png';
+import img7 from '../assets/images/7. Cucina a Vista.png';
+import img8 from '../assets/images/8. Cucina Hamburger.png';
+import img9 from '../assets/images/9. Parete in Jeans.png';
+import img10 from '../assets/images/10.ShopSoulRock.png';
+import img11 from '../assets/images/Sala Robocop.png';
 import img12 from '../assets/images/chatgpt-image-2025-08-02.png';
 
 const images = [
-  img1, img2, img3, img4, img5, img6,
-  img7, img8, img9, img10, img11, img12,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12
 ];
 
 const Gallery = () => {
