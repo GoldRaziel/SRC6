1266083b6d074931baae045b361a897ecd2cc8ad
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 19:56:44 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,13 @@ const Home = () => {
 
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <img src={logo} alt="Soul Rock Exterior" style={{ maxWidth: '100%', borderRadius: '10px' }} />
-      <h1 style={{ color: '#00ccff', marginTop: '2rem' }}>{t('home_title')}</h1>
-      <p style={{ fontSize: '1.2rem', maxWidth: '700px', margin: '0 auto' }}>{t('home_text')}</p>
+      <h1 style={{ fontSize: '2.5rem', marginBottom: '1rem' }}>
+        {t('home_title').split(' ')[0]}<br />
+        <img src={logo} alt="Soul Rock Logo" style={{ maxWidth: '300px', marginTop: '10px', borderRadius: '10px' }} />
+      </h1>
+      <p style={{ fontSize: '1.2rem', maxWidth: '700px', margin: '0 auto' }}>
+        {t('home_text')}
+      </p>
       <div style={{ marginTop: '2rem' }}>
         <a href="/idea"><button>{t('home_cta_discover')}</button></a>
         <a href="/donate"><button>{t('home_cta_donate')}</button></a>
