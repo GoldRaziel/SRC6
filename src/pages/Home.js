50f12913dd8b462b1f5ae748a97df597f5d48551
GoldRaziel <paolocimino21@gmail.com>
2025-08-05 20:13:42 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,11 +15,17 @@ const Home = () => {
       />
 
       {/* Titolo in tre righe */}
-      <h1 style={{ fontSize: '2.5rem', marginTop: '2rem', color: '#00ccff', lineHeight: '1.5' }}>
-        Benvenuti al<br />
-        SOUL ROCK<br />
-        MUSEUM RESTAURANT
-      </h1>
+      <div style={{ marginTop: '2rem', lineHeight: '1.5' }}>
+        <div style={{ fontSize: '2.5rem', color: '#ffffff' }}>
+          Benvenuti al
+        </div>
+        <div style={{ fontSize: '3rem', fontWeight: 'bold', color: '#00ccff' }}>
+          SOUL ROCK
+        </div>
+        <div style={{ fontSize: '2.25rem', color: '#00ccff' }}>
+          MUSEUM RESTAURANT
+        </div>
+      </div>
 
       {/* Frase */}
       <p style={{ fontSize: '1.2rem', maxWidth: '700px', margin: '0 auto' }}>
@@ -28,8 +34,12 @@ const Home = () => {
 
       {/* Pulsanti */}
       <div style={{ marginTop: '2rem' }}>
-        <a href="/idea"><button>{t('home_cta_discover')}</button></a>
-        <a href="/donate"><button>{t('home_cta_donate')}</button></a>
+        <a href="/investors">
+          <button style={{ fontSize: '1.2rem' }}>{t('home_cta_discover')}</button>
+        </a>
+        <a href="/donate">
+          <button style={{ fontSize: '1.2rem' }}>{t('home_cta_donate')}</button>
+        </a>
       </div>
     </div>
   );
